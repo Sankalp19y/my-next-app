78ba3d466e968a6382a44c054186b07a9fd3f61a
sankalp19y <sankalp.pachauri004@gmail.com>
2025-08-26 02:38:51 +0530

    Corrected next.config file name and syntax

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main>
       <h1>My Next.js App</h1>
-      <p>This is a simple page for my live call demonstration.</p>
+      <p>hello devop</p>
     </main>
   );
 }
